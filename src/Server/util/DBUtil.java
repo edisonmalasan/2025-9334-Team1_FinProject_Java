016a0e48f6986ccbeea9be74cc3fe53a3958bc98
Eruki <malasanedison1@gmail.com>
2025-04-10 00:08:21 +0800

    added DB Utility (Todo: Connection setup)

--- src/Server/util/DBUtil.java
+++ src/Server/util/DBUtil.java
@@ -1,4 +1,27 @@
 package Server.util;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
 public class DBUtil {
+    private static DataSource dataSource;
+
+    static {
+        // TODO: Connection setup
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void closeQuietly(Connection conn) {
+        if (conn != null) {
+            try { conn.close(); }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
