8b576559a2e474ae99362ffe0f19cfdfe7601e4c
DANNIEVIC LUACAN <2240879@slu.edu.ph>
2025-04-22 01:53:34 +0000

    Edit AdminRequestService.java

--- src/Server/service/AdminRequestService.java
+++ src/Server/service/AdminRequestService.java
@@ -6,8 +6,27 @@ import common.client.admin.AdminServicePOA;
 import common.referenceClasses.Admin;
 
 public class AdminRequestService extends AdminServicePOA {
+    // TODO: Implement DAO and other methods (Follow PlayerRequestService format)
+
     @Override
     public void request(AdminRequestType type, Admin admin, ClientCallback callback) {
+        if (type.equals(AdminRequestType.ADMIN_LOGIN)) {
 
+        } else if (type.equals(AdminRequestType.CREATE_NEW_PLAYER)) {
+
+        } else if (type.equals(AdminRequestType.GET_PLAYER_DETAILS)) {
+
+        } else if (type.equals(AdminRequestType.UPDATE_PLAYER_DETAILS)) {
+
+        } else if (type.equals(AdminRequestType.DELETE_PLAYER)) {
+
+        } else if (type.equals(AdminRequestType.SEARCH_PLAYER)) {
+
+        } else if (type.equals(AdminRequestType.SET_LOBBY_WAITING_TIME)) {
+
+        } else if (type.equals(AdminRequestType.SET_ROUND_TIME)) {
+
+        }
     }
 }
+
