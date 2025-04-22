73c8c0c37ecbfdd0884bac8dd72d9610039c85d2
Eruki <malasanedison1@gmail.com>
2025-04-22 20:54:34 +0800

    added password hashing in server/utility

---
+++ src/Server/util/PasswordUtility.java
@@ -0,0 +1,55 @@
+package Server.util;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class PasswordUtility {
+    private static final int ITERATIONS = 65536;
+    private static final int KEY_LENGTH = 256;
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA256";
+
+    public static String hashPassword(String password) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, ITERATIONS, KEY_LENGTH);
+
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(ALGORITHM);
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            return  ITERATIONS + ":" +
+                    Base64.getEncoder().encodeToString(salt) + ":" +
+                    Base64.getEncoder().encodeToString(hash);
+        } catch (Exception e) {
+            throw new RuntimeException("Hashing failed", e);
+        }
+    }
+
+    public static boolean verify(String password, String storedHash) {
+        String[] parts = storedHash.split(":");
+        int iterations = Integer.parseInt(parts[0]);
+        byte[] salt = Base64.getDecoder().decode(parts[1]);
+        byte[] hash = Base64.getDecoder().decode(parts[2]);
+
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, hash.length * 8);
+
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(ALGORITHM);
+            byte[] testHASH = factory.generateSecret(spec).getEncoded();
+            return slowEquals(hash, testHASH);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static boolean slowEquals(byte[] a, byte[] b) {
+        int diff = a.length ^ b.length;
+        for (int i = 0; i < a.length && i < b.length; i++) {
+            diff |= a[i] ^ b[i];
+        }
+        return diff == 0;
+    }
+}
