2644285268206d0338ff2901bf562b683ce579ed
Yuen, Ka Yue Ymanuelle T. <2235608@slu.edu.ph>
2025-04-26 01:54:41 +0800

    contents added in classes "AdminDAO"

--- src/Server/DataAccessObject/AdminDAO.java
+++ src/Server/DataAccessObject/AdminDAO.java
@@ -1,4 +1,57 @@
 package Server.DataAccessObject;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import Server.database.DatabaseConnection;
+
 public class AdminDAO {
+    /**
+     * Retrieves an existing account with the specified credentials.
+     * @param userName
+     * @param password
+     * @return
+     */
+    public static boolean checkPlayerAccount(String userName, String password) {
+        String query = "SELECT CONCAT(`prefix`,`id`) AS 'Player ID', `username`, `password` FROM `player` FROM player WHERE username = ? AND password = ?";
+        try (Connection con = DatabaseConnection.getConnection();
+             PreparedStatement prepstmt = con.prepareStatement(query)) {
+            prepstmt.setString(1, userName);
+            prepstmt.setString(2, password);
+            ResultSet res = prepstmt.executeQuery();
+            if (res.next())
+                return true;
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Edits the info of a particular player
+     * Returns true if the update is succesful, otherwise it returns falls
+     *
+     * @param username
+     * @param toEdit
+     * @param newInfo
+     * @return
+     */
+    public static boolean editPlayerInfo(String username, String toEdit, String newInfo) {
+        if (!(toEdit.equalsIgnoreCase("username"))) {
+            return false;
+        }
+        String query = "UPDATE player SET " + toEdit + " = ? WHERE username = ?";
+        try (Connection con = DatabaseConnection.getConnection();
+             PreparedStatement prepstmt = con.prepareStatement(query)){
+            prepstmt.setString(1, newInfo);
+            prepstmt.setString(2, username);
+            prepstmt.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
 }
