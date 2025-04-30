a4ac6f4885724385e0eb2a7db9154221a74fefdd
Eruki <malasanedison1@gmail.com>
2025-04-30 19:50:59 +0800

    player result skeleton

--- src/Client/Player/controller/PlayerResults.java
+++ src/Client/Player/controller/PlayerResults.java
@@ -1,4 +1,25 @@
 package Client.Player.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
 public class PlayerResults {
+
+    @FXML
+    private Button backToMainMenuButton;
+
+    @FXML
+    private Label gameResult;
+
+    @FXML
+    private Label playerName;
+
+    @FXML
+    private Label playerPoints;
+
+    @FXML
+    private Pane resultDisplay;
+
 }
