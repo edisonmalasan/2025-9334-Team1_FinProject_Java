a361c9e5f4c23dbd15582a91facc10d10c600fd6
Personal <2240879@slu.edu.ph>
2025-05-01 20:31:51 +0800

    Added register logic for player

--- src/Client/Player/controller/PlayerRegister.java
+++ src/Client/Player/controller/PlayerRegister.java
@@ -1,7 +1,13 @@
 package Client.Player.controller;
 
+import Client.WhatsTheWord.client.player.PlayerRequestType;
+import Client.WhatsTheWord.client.player.PlayerService;
+import Client.WhatsTheWord.referenceClasses.Player;
+import Client.main.Client;
+import Server.DataAccessObject.PlayerDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -19,7 +25,7 @@ public class PlayerRegister {
 
     @FXML
     private TextField usernameTextField;
-
+    private PlayerService playerService = Client.playerService;
     @FXML
     void handleBackLink(ActionEvent event) {
 
@@ -27,7 +33,25 @@ public class PlayerRegister {
 
     @FXML
     void handleEnter(ActionEvent event) {
+        String username = usernameTextField.getText();
+        String password = passwordField.getText();
 
+        // Simple validation to check if username or password is empty
+        if (username.isEmpty() || password.isEmpty()) {
+            showAlert("Error", "Both username and password are required!");
+        } else {
+            Player newPlayer= new Player(0, username, password, 0, 0, 0, false);
+            playerService.request(PlayerRequestType.LOGIN, newPlayer, Client.callback);
+            showAlert("Success", "Registration Successful!");
+        }
+    }
+
+    // Helper method to show an alert pop-up
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
 }
