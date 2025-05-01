14c59c11214a550a82379bc1eb4fa8276e8e1f8c
Personal <2240879@slu.edu.ph>
2025-05-01 20:32:13 +0800

    Modified database connection

--- src/Server/database/DatabaseConfig.java
+++ src/Server/database/DatabaseConfig.java
@@ -3,7 +3,7 @@ package Server.database;
 import java.util.Properties;
 
 public class DatabaseConfig {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/whatstheword";
     public static final String DB_USER = "root";
     public static final String DB_PASSWORD = "";
     public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
--- src/Server/database/DatabaseConnection.java
+++ src/Server/database/DatabaseConnection.java
@@ -10,15 +10,15 @@ public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
             try {
-                Class.forName(DatabaseConfig.DB_DRIVER);
+                //Class.forName(DatabaseConfig.DB_DRIVER);
                 connection = DriverManager.getConnection(
                         DatabaseConfig.DB_URL,
                         DatabaseConfig.DB_USER,
                         DatabaseConfig.DB_PASSWORD
                 );
-            } catch (ClassNotFoundException e) {
-                System.err.println("JDBC Driver Not Found");
-                e.printStackTrace();
+//            } catch (ClassNotFoundException e) {
+//                System.err.println("JDBC Driver Not Found");
+//                e.printStackTrace();
             } catch (SQLException e) {
                 System.err.println("SQL Error: " + e.getMessage());
                 throw e;
