b8072acb11004b5710c3d3a1bc5e21461f1fdff5
Edison Malasan <2245504@slu.edu.ph>
2025-05-03 11:29:29 +0800

    changes for admin view

--- src/Client/Admin/controller/AdminViewController.java
+++ src/Client/Admin/controller/AdminViewController.java
@@ -1,8 +1,87 @@
 package Client.Admin.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class AdminViewController {
-    public void handleBack(ActionEvent actionEvent) {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button addButton111;
+
+    @FXML
+    private Label adminNameLabel;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TextField gameTimeTextField;
+
+    @FXML
+    private TableView<?> playersTable;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private Button setGameTimeButton;
+
+    @FXML
+    private Button setWaitingTimeButton;
+
+    @FXML
+    private TableColumn<?, ?> userNameColumn;
+
+    @FXML
+    private TextField waitingTimeTextField;
+
+    @FXML
+    private TableColumn<?, ?> winsColumn;
+
+    @FXML
+    void handleAddPlayer(ActionEvent event) {
+
     }
+
+    @FXML
+    void handleDeletePlayer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRefresh(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSavePlayer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSetGameTime(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSetWaitingTime(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sortByScore(ActionEvent event) {
+
+    }
+
 }
